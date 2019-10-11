dc82369d099864a71dc3c3b1237e433b4f5364cb
Himanshu Choudhary <himnshu@iitk.ac.in>
2019-10-12 00:54:33 +0530

    feat/postgres-integration (#4)
    * Added postgres dependencies, heroku add-on config and docker-compose config
    
    * Disabled metadata defaults for postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,22 @@
 
 set -x
 
-sed -i "s#&&port&&#$PORT#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
+# Extracting values from DB URL
+proto="`echo $DATABASE_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
+url=`echo $DATABASE_URL | sed -e s,$proto,,g`
+userpass="`echo $url | grep @ | cut -d@ -f1`"
+dbusername=`echo $userpass | grep : | cut -d: -f1`
+dbpassword=`echo $userpass | grep : | cut -d: -f2`
+hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
+dbhost=`echo $hostport | grep : | cut -d: -f1`
+dbport=`echo $hostport | grep : | cut -d: -f2`
+dbname="`echo $url | grep / | cut -d/ -f2-`"
 
-cat /app/application-${SPRING_PROFILES_ACTIVE}.yml
+sed -i "s#&&port&&#$PORT#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
+sed -i "s#&&dbhost&&#$dbhost#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
+sed -i "s#&&dbport&&#$dbport#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
+sed -i "s#&&dbname&&#$dbname#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
+sed -i "s#&&dbusername&&#$dbusername#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
+sed -i "s#&&dbpassword&&#$dbpassword#g" /app/application-${SPRING_PROFILES_ACTIVE}.yml
 
 exec mvn spring-boot:run -f /app/pom.xml
